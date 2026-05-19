39548d0115813b06d68beda2b73a729d0f9c9e28
crosszay1 <Isaiahbfreundlich@Gmail.com>
2026-05-18 20:02:01 -0400

    It is staritng to come to light...

---
+++ train.py
@@ -0,0 +1,34 @@
+import tiktoken #cool tokenizer by openai
+
+def user_input():
+    question = input("Enter your prompt: ")
+    return question
+
+def construct_prompt(question):
+    prompt = f"""
+    Question: {question}\n
+    Answer:
+    """
+    return prompt
+
+
+def generate_training_data(data, n, tokenizer):
+    tokens = tokenizer.encode(data)
+    X = []
+    y = []
+    for i in range(len(tokens) - n):
+      X.append(tokens[i : n + i])
+      y.append(tokens[i + 1 : n + i + 1])
+
+    return [X, y]
+#X = input, y = output
+#Model tries to predict y given X.
+
+
+
+#For now we just train on one data piece
+data = ""
+with open("data/CIA_World_Fact_Sheet_2006.txt", "r") as f:
+  data = f.read()
+
+encoding = tiktoken.get_encoding("o200k_base")
