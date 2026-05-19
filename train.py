c877850f6badf0f04f853f6392801fd0cfd6de40
crosszay1 <Isaiahbfreundlich@Gmail.com>
2026-05-18 20:05:19 -0400

    Continuing on...

--- train.py
+++ train.py
@@ -1,4 +1,14 @@
 import tiktoken #cool tokenizer by openai
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+import torch.nn as nn
+import torch.optim as optim
+
+
+#        config stuffs        #
+sequence_len = 5
+
+
 
 def user_input():
     question = input("Enter your prompt: ")
@@ -32,3 +42,20 @@ with open("data/CIA_World_Fact_Sheet_2006.txt", "r") as f:
   data = f.read()
 
 encoding = tiktoken.get_encoding("o200k_base")
+
+
+X, Y = generate_training_data(data, sequence_len, encoding)
+
+
+tensor_X = torch.tensor(X, dtype = torch.long)
+tensor_y = torch.tensor(Y, dtype = torch.long)
+
+tensor_X = tensor_X.to("cuda")
+tensor_y = tensor_y.to("cuda")
+
+tensor_X.shape, tensor_y.shape
+
+
+
+dataset = TensorDataset(tensor_X, tensor_y)
+dataloader = DataLoader(dataset, batch_size=256, shuffle=True)
